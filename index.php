b6424f4585eb74785729dc4d6d275081063b581d
Shan Jiang <jiang_shan@live.com>
2018-04-13 21:43:42 -0400

    set up home careers

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ include('sections/header.html');
 include('sections/home-banner.html');
 include('sections/home-why.html');
 include('sections/home-stories.html');
+include('sections/home-careers.html');
 
 ?>
 
