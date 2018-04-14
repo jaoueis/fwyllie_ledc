cf4e966b6814afedb708b0fb7557296efcd14122
Shan Jiang <jiang_shan@live.com>
2018-04-14 12:48:59 -0400

    set up home lifestyle

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include('sections/home-banner.html');
 include('sections/home-why.html');
 include('sections/home-stories.html');
 include('sections/home-careers.html');
+include('sections/home-lifestyle.html');
 
 ?>
 
