4bc352aa6eceb9c82e0e28b6ef06810e3f925183
Shan Jiang <jiang_shan@live.com>
2018-04-15 01:34:06 -0400

    set up resident indicator

--- why-london.php
+++ why-london.php
@@ -3,5 +3,6 @@
 include('sections/header.html');
 include('sections/why-banner.html');
 include('sections/why-economic.html');
+include('sections/why-resident.html');
 
 include('sections/footer.html');
