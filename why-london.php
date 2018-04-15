10aaae985a47009b3febd130cceaaf5b14139f71
Shan Jiang <jiang_shan@live.com>
2018-04-15 02:46:04 -0400

    set up why london page

--- why-london.php
+++ why-london.php
@@ -4,5 +4,6 @@ include('sections/header.html');
 include('sections/why-banner.html');
 include('sections/why-economic.html');
 include('sections/why-resident.html');
+include('sections/why-development.html');
 
 include('sections/footer.html');
