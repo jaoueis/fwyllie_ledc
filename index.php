5f13e2f461b02c258552dd4aa564ac741a8dacf0
Shan Jiang <jiang_shan@live.com>
2018-04-16 01:43:58 -0400

    set up newsletter

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+require_once("admin/newsletter.php");
+if (isset($_POST['submit'])) {
+    $subEmailAdd = trim($_POST['email']);
+    if (!empty($subEmailAdd)) {
+        $sub     = 0;
+        $result  = saveEmailAdd($subEmailAdd, $sub);
+        $message = $result;
+    }
+}
 
 include('sections/header.html');
 include('sections/home-banner.html');
