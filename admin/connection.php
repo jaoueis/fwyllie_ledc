b171eb21da9ccedafa76163b43ec1cbb1335d661
Shan Jiang <jiang_shan@live.com>
2018-04-16 01:36:33 -0400

    set up contact page

--- admin/connection.php
+++ admin/connection.php
@@ -3,8 +3,9 @@ $host_name = "localhost";
 $database  = "ledc";
 $user_name = "root";
 $password  = "root";
+$port      = 8889;
 date_default_timezone_set("America/Toronto");
-$link = mysqli_connect($host_name, $user_name, $password, $database);
+$link = mysqli_connect($host_name, $user_name, $password, $database, $port);
 
 if (mysqli_connect_errno()) {
     echo "<p>Verbindung zum MySQL Server fehlgeschlagen: " . mysqli_connect_error() . "</p>";
---
+++ contact.php
@@ -0,0 +1,5 @@
+<?php
+include("sections/header.html");
+include("sections/contact-banner.html");
+include("sections/contact.html");
+include("sections/footer.html");
